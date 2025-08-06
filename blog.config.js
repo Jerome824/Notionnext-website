70224e8a48015ab62799e4c04f3d6a09491c427c
Jerome824 <1014831018@qq.com>
2025-08-06 16:45:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hello, it's Jerome, welcome to my blog🎉',
+    'Hello, it is Jerome, welcome to my blog🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
